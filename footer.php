5a5b5e469bb59363ae030ba69b5fb3878388d9ad
REDiowave <boschdoor.ian@gmail.com>
2019-11-20 15:18:03 -0800

    more styling

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
 		<?php wp_nav_menu(array('theme_location' => 'primary', 'menu_id' => 'primary-menu')); ?>
 	</nav><!-- #site-navigation -->
 	<div class="site-info">
-		
+		<p>Brought to you by <a href="http://www.redacademy.com/">RED Academy</a></p>
 	</div><!-- .site-info -->
 </footer><!-- #colophon -->
 </div><!-- #page -->
--- js/qod.js
+++ js/qod.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 (function($){
 
   // $('body').append('Hello World');
@@ -6,11 +7,11 @@
   // 1. get request to grab random post and append to the dom
     
     // Add click event for "Show me another" button and then run ajax code below
-      $.ajax({
-      method: "GET",
-      url: // qod.vars.rest_url + /wp/vs/posts?filter[orderby]=rand&filter[posts_per_page]=1
+    $.ajax({
+      method: 'GET'
+      // url: qod.vars.rest_url + /wp/vs/posts?filter[orderby]=rand&filter[posts_per_page]=1
     })
-    $.ajax().done(function(data){
+    .done(function(data){
       console.log(data);
     }).fail(function(error){
       console.log(error);
